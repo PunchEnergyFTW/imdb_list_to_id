ceba4cc9da656f8547bdfc5fe00cf6f5b4980ae4
PunchEnergyFTW <61878903+PunchEnergyFTW@users.noreply.github.com>
2022-08-25 21:03:53 +0200

    Add files via upload
    Added the Script

---
+++ imdb-scraper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+read -p "Link der Liste: " starturl
+while true
+do
+nexturl=$(lynx -dump "$starturl" | grep -o 'https://www.imdb.com/search/title/.*=adv_nxt' | head -1)
+lynx -dump "$starturl" | grep -o 'https://www.imdb.com/title/tt.*/?ref_=adv_li_tt' | sed 's-https://www.imdb.com/title/--g' | sed 's-/?ref_=adv_li_tt--g' >> imdb-ids.txt
+echo $starturl >> scraped-links.txt
+if [ "$starturl" == "$nexturl" ]
+	then
+	echo Starturl is the same as Nexturl
+	exit
+fi
+starturl=$nexturl
+number=$(($number + 1))
+if [ -z "$starturl" ]
+	then
+		echo "No Next Button found! Probably the last Site - YES is was scraped."
+		exit
+	else
+		echo Done scraping Link number: $number
+fi
+done
